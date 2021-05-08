ae838ccb58869a9967ff0ff7d165ee14fc6c971b
Alamin <alaminprogramerr@gmail.com>
2021-05-08 14:52:11 +0600

    added

---
+++ client/src/helpers/authHelper.js
@@ -0,0 +1,48 @@
+import jwtDecode from 'jwt-decode';
+import React from 'react';
+import { Route, Redirect } from 'react-router-dom';
+
+const ProtectedRoute = ({
+  component: Component,
+  roles :roles,
+  ...rest
+}) => {
+  const setComponent = (props) => {
+    if (true) {
+      const currentUser ={
+        role:'Admin',
+      }
+      if (true) {        
+        // const token=jwtDecode(true)
+        if (true) {
+          if (true) {
+            return <Component {...props} />;
+          }
+          return (
+            <Redirect
+              to={{
+                pathname: '/unauthorized',
+                state: { from: props.location },
+              }}
+            />
+          );
+        }
+        return <Component {...props} />;
+      }
+      return (
+        <Redirect
+          to={{
+            pathname: '/user/login',
+            state: { from: props.location },
+          }}
+        />
+      );
+    }
+    return <Component {...props} />;
+  };
+
+  return <Route {...rest} render={setComponent} />;
+};
+
+// eslint-disable-next-line import/prefer-default-export
+export { ProtectedRoute };
